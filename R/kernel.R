618aeedc64f0c0868c8e96b5f777ac9b7322cb6a
Kate Noel <noel0047@umn.edu>
2021-05-28 10:37:05 -0500

    Add save imputed data to specified file

--- R/kernel.R
+++ R/kernel.R
@@ -4,7 +4,7 @@
 #' @param data A 450k feature data set with CpG sites as columns
 #' @return
 #' @export
-kernel <- function(data,chrom) {
+kernel <- function(data,chrom,path) {
   # load in training data
   x_train <- get(noquote(paste('chr',chrom,'_xtrain', sep = "")), envir = asNamespace('missingmethyl'), inherits = FALSE)
   y_train <- get(noquote(paste('chr',chrom,'_ytrain', sep = "")), envir = asNamespace('missingmethyl'), inherits = FALSE)
@@ -21,6 +21,8 @@ kernel <- function(data,chrom) {
   rbfker <- kernlab::rbfdot(sigma = sig)
   y_pred <- kernel_pred(x_train,y_train,x_test,ker=rbfker,alpha=0.99)
 
-  # return 450k feature set + imputed EPIC probes
-
+  # save 450k feature set + imputed EPIC probes to specified file location
+  EPIC <- cbind(data, y_pred)
+  save_location <- paste(path,'chr',chrom,'EPIC',sep='')
+  saveRDS(EPIC, save_location)
 }
