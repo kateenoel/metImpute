fa5b3ceb2c9e9d914fee58b2ce9ca2f195a04280
Kate Noel <noel0047@umn.edu>
2021-06-02 10:59:16 -0500

    Add source package to function calls and update some coding style

--- R/helpers.R
+++ R/helpers.R
@@ -1,20 +1,19 @@
-train_psi_mat = function(x, ker){
-
-  kermat =kernelMatrix(ker, x)
-  n = ncol(kermat)
-  eigres = eigen(kermat)
-  psi_mat = eigres$vectors
+train_psi_mat <- function(x, ker){
+  kermat <- kernlab::kernelMatrix(ker, x)
+  n <- ncol(kermat)
+  eigres <- eigen(kermat)
+  psi_mat <- eigres$vectors
   return(list(psi_mat=psi_mat,lambda=eigres$values,ker=ker))
 }
 
-pred_psi_mat = function(xp,x_train,psi_mat,lambda,ker){
-  kp=kernelMatrix(ker,xp,x_train)
-  psi_p = kp%*%psi_mat/matrix(lambda,nrow=nrow(kp),ncol=length(lambda),byrow=TRUE)
+pred_psi_mat <- function(xp,x_train,psi_mat,lambda,ker){
+  kp <- kernlab::kernelMatrix(ker,xp,x_train)
+  psi_p <- kp%*%psi_mat/matrix(lambda,nrow=nrow(kp),ncol=length(lambda),byrow=TRUE)
   return(psi_p)
 }
 
 
-scatter_pred = function(y_pred,y_test,idx = 1,pch=19,cex=0.5,main=NULL,ylim=NULL,xlim=NULL){
+scatter_pred <- function(y_pred,y_test,idx = 1,pch=19,cex=0.5,main=NULL,ylim=NULL,xlim=NULL){
   plot(y_pred[,1],y_test[,1],ylim=ylim,xlim=xlim,
        pch=pch,xlab="Prediction",ylab="Observation",cex=cex,main=main)
   if(length(idx)>1)
@@ -24,26 +23,24 @@ scatter_pred = function(y_pred,y_test,idx = 1,pch=19,cex=0.5,main=NULL,ylim=NULL
   abline(a=0,b=1)
 }
 
-rbf_kernel_pred = function(x_train,y_train,x_test,sigma1 = 0.0001){
+rbf_kernel_pred <- function(x_train,y_train,x_test,sigma1 = 0.0001){
   rbf <- rbfdot(sigma = sigma1)
-  train_fit = train_psi_mat(x_train,rbf)
-  test_psi_mat = pred_psi_mat(x_test,x_train,train_fit$psi_mat,lambda=train_fit$lambda,ker=train_fit$ker)
-  beta_coef=t(train_fit$psi_mat)%*%y_train
-  y_pred = test_psi_mat%*%beta_coef
+  train_fit <- missingmethyl::train_psi_mat(x_train,rbf)
+  test_psi_mat <- missingmethyl::pred_psi_mat(x_test,x_train,train_fit$psi_mat,lambda=train_fit$lambda,ker=train_fit$ker)
+  beta_coef <- t(train_fit$psi_mat)%*%y_train
+  y_pred <- test_psi_mat%*%beta_coef
   return(y_pred)
 }
 
 
-kernel_pred = function(x_train,y_train,x_test,ker,alpha=0.95,output=NULL){
-  train_fit = train_psi_mat(x_train,ker)
-  contr = cumsum(train_fit$lambda)/sum(train_fit$lambda)
-  idx = min(which(contr>alpha))
-
-  test_psi_mat = pred_psi_mat(x_test,x_train,train_fit$psi_mat[,1:idx],
+kernel_pred <- function(x_train,y_train,x_test,ker,alpha=0.95,output=NULL){
+  train_fit <- missingmethyl::train_psi_mat(x_train,ker)
+  contr <- cumsum(train_fit$lambda)/sum(train_fit$lambda)
+  idx <- min(which(contr>alpha))
+  test_psi_mat <- missingmethyl::pred_psi_mat(x_test,x_train,train_fit$psi_mat[,1:idx],
                               lambda=train_fit$lambda[1:idx],ker=train_fit$ker)
-
-  beta_coef=t(train_fit$psi_mat[,1:idx])%*%y_train
-  y_pred = test_psi_mat%*%beta_coef
+  beta_coef <- t(train_fit$psi_mat[,1:idx])%*%y_train
+  y_pred <- test_psi_mat%*%beta_coef
   if(is.null(output)){
     return(y_pred)
   }
@@ -51,12 +48,12 @@ kernel_pred = function(x_train,y_train,x_test,ker,alpha=0.95,output=NULL){
               test_psi_mat=test_psi_mat,lambda=train_fit$lambda,contr=contr,contr_idx=idx))
 }
 
-summarize_res = function(y_pred,y_test){
-  rmse = sapply_pb(1:ncol(y_test),function(i) sqrt(mean((y_pred[,i]-y_test[,i])^2,na.rm=TRUE)))
-  R = sapply_pb(1:ncol(y_test),function(i) cor(y_pred[,i],y_test[,i],use="pairwise.complete.obs"))
-  R2 = R*R
-  mae = sapply_pb(1:ncol(y_test), function(i) mean(abs(y_pred[,i]-y_test[,i])))
-  tab = c(rmse=mean(rmse,na.rm=TRUE),R2=mean(R2,na.rm=TRUE), mae=mean(mae, na.rm=TRUE))
+summarize_res <- function(y_pred,y_test){
+  rmse <- missingmethyl::sapply_pb(1:ncol(y_test),function(i) sqrt(mean((y_pred[,i]-y_test[,i])^2,na.rm=TRUE)))
+  R <- missingmethyl::sapply_pb(1:ncol(y_test),function(i) cor(y_pred[,i],y_test[,i],use="pairwise.complete.obs"))
+  R2 <- R*R
+  mae <- missingmethyl::sapply_pb(1:ncol(y_test), function(i) mean(abs(y_pred[,i]-y_test[,i])))
+  tab <- c(rmse=mean(rmse,na.rm=TRUE),R2=mean(R2,na.rm=TRUE), mae=mean(mae, na.rm=TRUE))
   return(list(rmse=rmse,R2=R2,mae=mae,tab=tab))
 }
 
