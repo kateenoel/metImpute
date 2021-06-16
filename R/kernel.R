ac4018b0cbb68070ce2c82397f559a6544525a45
Kate Noel <noel0047@umn.edu>
2021-06-16 14:02:52 -0500

    Stylistic updates to single-line if statements and for loops and remove HM450 output

--- R/kernel.R
+++ R/kernel.R
@@ -13,24 +13,14 @@
 kernel <- function(data450, data850, chrom, path, sigma = NULL) {
   # special case for sex chromosomes
   chrom_val <- NULL
-  if (chrom == 'X') {
-    chrom_val <- 23
-  }
-  if (chrom == 'Y') {
-    chrom_val <- 24
-  }
-  else {
-    chrom_val <- chrom
-  }
+  if (chrom == 'X') {chrom_val <- 23}
+  if (chrom == 'Y') {chrom_val <- 24}
+  else {chrom_val <- chrom}
 
    # impute missing values with the column mean (source data and user data)
-  for(i in 1:ncol(data850)) {
-    data850[is.na(data850[,i]), i] <- mean(data850[,i], na.rm = TRUE)
-  }
+  for(i in 1:ncol(data850)) {data850[is.na(data850[,i]), i] <- mean(data850[,i], na.rm = TRUE)}
 
-  for(i in 1:ncol(data450)) {
-    data450[is.na(data450[,i]), i] <- mean(data450[,i], na.rm = TRUE)
-  }
+  for(i in 1:ncol(data450)) {data450[is.na(data450[,i]), i] <- mean(data450[,i], na.rm = TRUE)}
 
   # split data850 into x_train (450k feature set) and y_train (EPIC only feature set)
   indicator <- metImpute::indicatordata
@@ -60,10 +50,6 @@ kernel <- function(data450, data850, chrom, path, sigma = NULL) {
   EPIC <- y_pred
   EPIC_save_path <- paste(path,'/chr',chrom,'_EPIC.rds',sep='')
   saveRDS(EPIC, EPIC_save_path)
-
-  # save and export user data (x_test) with missing values imputed to specified file location
-  data450_save_path <- paste(path,'/chr',chrom,'_data450.rds',sep='')
-  saveRDS(x_test, data450_save_path)
 
   # save and export summary statistics table
   summary_stats_save_path <- paste(path, '/chr', chrom, '_summarystats.rds',sep='')
--- R/ridge.R
+++ R/ridge.R
@@ -14,13 +14,9 @@ ridge <- function(data450, data850, chrom, path) {
   lam <-  get(noquote(paste('chr',chrom,'_lambdas', sep = '')), envir = asNamespace('metImpute'), inherits = FALSE)
 
   # impute missing values with the column mean (source data and user data)
-  for(i in 1:ncol(data850)) {
-    data850[is.na(data850[,i]), i] <- mean(data850[,i], na.rm = TRUE)
-  }
+  for(i in 1:ncol(data850)) {data850[is.na(data850[,i]), i] <- mean(data850[,i], na.rm = TRUE)}
 
-  for(i in 1:ncol(data450)) {
-    data450[is.na(data450[,i]), i] <- mean(data450[,i], na.rm = TRUE)
-  }
+  for(i in 1:ncol(data450)) {data450[is.na(data450[,i]), i] <- mean(data450[,i], na.rm = TRUE)}
 
   # split data850 into x_train (450k feature set) and y_train (EPIC only feature set)
   indicator <- metImpute::indicatordata
@@ -50,10 +46,6 @@ ridge <- function(data450, data850, chrom, path) {
   EPIC <- y_pred
   EPIC_save_path <- paste(path,'/chr',chrom,'_EPIC.rds',sep='')
   saveRDS(EPIC, EPIC_save_path)
-
-  # save and export user data (x_test) with missing values imputed to specified file location
-  data450_save_path <- paste(path,'/chr',chrom,'_data450.rds',sep='')
-  saveRDS(x_test, data450_save_path)
 
   # save and export summary statistics table
   summary_stats_save_path <- paste(path, '/chr', chrom, '_summarystasts.rds',sep='')
